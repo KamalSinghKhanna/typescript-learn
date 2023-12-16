da0d9f921126d30bf249efe34b33d812a2afd362
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2023-12-16 11:53:38 +0530

    context api and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import {FC} from "react";
+import {FC, createContext} from "react";
 import "./App.css";
 import { Person, HairColor } from "./components/Person";
 
+interface AppContextInterface {
+  name: string;
+  age: number;
+  country: string;
+}
+const AppContext = createContext<AppContextInterface | null>(null);
+
 const App: FC = () => {
-  return <>
-  <Person name="Kamal" age={28} email="kamal@gmail.com" hairColor={HairColor.pink} />
-  </>;
+  const contextValue: AppContextInterface = {
+    name: "Kamal",
+    age:21,
+    country: "India"
+  }
+  return (
+    <>
+      <AppContext.Provider value={contextValue}>
+        <Person
+          name="Kamal"
+          age={28}
+          email="kamal@gmail.com"
+          hairColor={HairColor.pink}
+        />
+      </AppContext.Provider>
+    </>
+  );
 }
 
 export default App;
